b0b395179269efd85196718994c168fe92f1677c
Whitneyz <66369610+whitneyz@users.noreply.github.com>
2020-11-10 12:30:27 +0100

    assignment until session

--- src/Controller/LearningController.php
+++ src/Controller/LearningController.php
@@ -9,21 +9,59 @@ use Symfony\Component\Routing\Annotation\Route;
 class LearningController extends AbstractController
 {
     /**
-     * @Route("/learning", name="learning") //name should always be unique
+     * @Route("/", name="learning") //name should always be unique
      */
     public function index(): Response
     {
         return $this->render('learning/index.html.twig', [
-            'name' => 'LearningController',
+            'controller_name' => 'LearningController',
         ]);
     }
+
     /**
      * @Route("/aboutme", name="aboutme") //name should always be unique
      */
-    public function aboutme(): Response
+    public function aboutMe(): Response
     {
         return $this->render('learning/aboutme.html.twig', [
             'name' => 'LearningController',
         ]);
     }
+
+    /**
+     * @Route("/showmyname", name="showname") //name should always be unique
+     */
+    public function showName(): Response
+    {
+        $name = "";
+        if (empty($_POST["fname"])) {
+            return $this->render('learning/changemyname.html.twig', [
+                'name' => 'Unknown',]);
+        }
+    }
+    /**
+     * @Route("/changemyname", name="changename") //name should always be unique
+     */
+    public function changeName(): Response
+    {
+        if ($name)["fname"]) {
+            return $this->render('learning/changemyname.html.twig', [
+                'name' => 'Unknown',]);
+        $_SESSION["fname"] = $name;
+        }
+    }
+
 }
+
+   /* /**
+     * @Route("/changemyname", name="changename") //name should always be unique
+
+    public
+    function changeName(): Response
+        $name = ["fname"];
+
+            return $this->render('learning/changemyname.html.twig', [
+                'name' => 'LearningController',
+            ]);
+        }*/
+
